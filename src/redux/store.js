16c6a0f97779782b590006101f074b8494780b32
Eduard <sorokolietov@gmail.com>
2023-12-02 14:58:57 +0100

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 import { authReducer } from "./auth/auth.reducer";
